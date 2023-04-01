35d03384ee95d274d0f76159c63c1edca8d95241
muhammed <muhammedyildiz.maras@gmail.com>
2023-04-01 23:15:55 +0300

    what?

--- models/basket.go
+++ models/basket.go
@@ -3,5 +3,5 @@ package models
 type Basket struct {
 	Username  string  `json:"username"`
 	ProductId int64   `json:"product_id"`
-	Product   Product `gorm:"foreignKey:ProductId; constraint:OnUpdate:CASCADE,OnDelete:DELETE;"`
+	Product   Product `gorm:"foreignKey:ProductId;"`
 }
